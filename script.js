b4a0d821e8732647c0bcae0ef9c74cf6616360d1
mdasjad9151 <asjad.ajic@gmail.com>
2025-05-21 01:50:52 +0530

    whiout animation

---
+++ script.js
@@ -0,0 +1,31 @@
+const typeTarget = document.getElementById("typewriter");
+
+// Break the text into parts
+const before = "Hi, I'm ";
+const name = "Mohd Asjad";
+
+let i = 0;
+let j = 0;
+
+function typeWriter() {
+  if (i < before.length) {
+    typeTarget.innerHTML += before.charAt(i);
+    i++;
+    setTimeout(typeWriter, 80);
+  } else if (j < name.length) {
+    // Add characters inside span
+    if (j === 0) {
+      typeTarget.innerHTML += `<span class="highlight-name">`;
+    }
+
+    typeTarget.innerHTML = typeTarget.innerHTML.replace(
+      /<span class="highlight-name">.*?<\/span>?$/,
+      `<span class="highlight-name">${name.substring(0, j + 1)}</span>`
+    );
+
+    j++;
+    setTimeout(typeWriter, 80);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", typeWriter);
